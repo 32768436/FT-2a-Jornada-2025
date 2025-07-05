aba304287ee5a0d964d621ef9aa6bd01292c6412
32768436 <anderson.morroque@escola.pr.gov.br>
2025-07-05 14:29:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ let linguagens = ["JavaScript","Python","C"];​
 item.textContent = linguagens[0];
 item.textContent = linguagens[1];
 item.textContent = linguagens[2];
+function inserirNome(){
+ let nomeUsuario = prompt("Qual o seu nome");
+ let elemento = document.querySelector("#nome-usuario");
+ elemento.textContent = nomeUsuario;
+}
+inserirNome();
