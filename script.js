c22ed244910a72eb2d22702d495dc8dfe3476b27
32768436 <anderson.morroque@escola.pr.gov.br>
2025-07-05 14:05:05 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const item = document.querySelector("#lista");
+item.textContent = linguagens[0];
+let linguagens = ["JavaScript","Python","C"];​
+item.textContent = linguagens[0];
+item.textContent = linguagens[1];
+item.textContent = linguagens[2];
